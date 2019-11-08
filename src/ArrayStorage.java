e11da6e08ab1b8256996e96d21163f8891594398
pilgrim04gm <pilgrim04@gmail.com>
2019-11-08 22:48:22 +0300

    first version of ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,62 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        int i = 0;
+        while (i < size()) {
+            if (storage[i].uuid.equals(uuid)) {
+                break;
+            } else {
+                i++;
+            }
+        }
+        return storage[i];
     }
 
     void delete(String uuid) {
+        int i = 0;
+        while (i < size()) {
+            if (storage[i].uuid.equals(uuid)) {
+                break;
+            } else {
+                i++;
+            }
+        }
+        for (int j = i; j < size() - 1; j++) {
+            storage[j] = storage[j + 1];
+        }
+        storage[size() - 1] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] getArray = new Resume[size()];
+        for (int i = 0; i < size(); i++) {
+            getArray[i] = storage[i];
+        }
+        return getArray;
     }
 
     int size() {
-        return 0;
+        int i = 0;
+        while (i < 10000) {
+            if (storage[i] == null) {
+                break;
+            } else {
+                i++;
+            }
+        }
+        return i;
     }
 }
